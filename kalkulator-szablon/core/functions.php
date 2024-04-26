9f1a91da9ea08c2a1c4798c52392dfd288d52ce8
ssjot <72383129+ssjot@users.noreply.github.com>
2024-04-26 02:09:45 +0200

    Delete kalkulator-szablon directory

--- kalkulator-szablon/core/functions.php
+++
@@ -1,5 +0,0 @@
-<?php
-function getFromRequest($param_name){
-	return isset($_REQUEST [$param_name]) ? $_REQUEST [$param_name] : null;
-}
-
--- kalkulator-szablon/index.php
+++
@@ -1,8 +0,0 @@
-<?php
-require_once dirname(__FILE__).'/init.php';
-
-//przekierowanie przeglądarki klienta (redirect)
-// header("Location: "._APP_URL."/app/calc.php");
-
-//przekazanie żądania do następnego dokumentu ("forward")
-include $conf->root_path.'/ctrl.php';
